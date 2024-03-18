e7a9d0dab72aecb3467c7bb768d3065a1c1ecb8c
Viktoriia Babaieva <145108530+Babakamaker@users.noreply.github.com>
2024-03-18 18:17:01 +0200

    Add files via upload

---
+++ Line.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace straight_line
+{
+    /*
+ 13.	Створити об’єкт класу „пряма  ”. Визначити за допомогою методу класу, чи належить одна із двох введених користувачем точок.
+ */
+    internal class Line
+    {
+        private double a, b, c;
+
+        public Line(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public bool IsOnLine(double x, double y)
+        {
+            return a * x + b * y + c == 0;
+        }
+    }
+}
