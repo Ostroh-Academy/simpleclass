9946d764419481c2fb07d858d5039ca2c873edae
Marian-Zharchynskyi <145098603+Marian-Zharchynskyi@users.noreply.github.com>
2024-03-18 18:14:02 +0200

    Add files via upload

---
+++ Lab1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArifmeticalProgression arifmeticalProgression = new ArifmeticalProgression(2,3,2);
+            double sum = arifmeticalProgression.SumOfProgression();
+            Console.WriteLine(sum);
+        }
+    }
+}
