a01a101a8a777989e1cea5e9e35306d75b224026
taimg1 <oleg.s2822@gmail.com>
2024-03-18 18:22:43 +0200

    Done Task
    V25

---
+++ PatternPrograming/Lab1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func func = new Func(1, 2);
+            Console.Write("Enter x: ");
+            double x = int.Parse(Console.ReadLine());
+            Console.WriteLine(func.CalculateFunc(x));
+            Console.WriteLine(func.IsMaxPoint(x) ? $"x: {x} is max value" : $"x: {x} isn`t max value");
+        }
+    }
+}
