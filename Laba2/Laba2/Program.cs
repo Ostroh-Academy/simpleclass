e619589f7e8c9f51cb6bfa1dfc15af5fbeb9d435
Andrew <shikigamori@gmail.com>
2024-03-24 23:28:22 +0200

    Виконав лаб. 2 (Варіант 8)

---
+++ Laba2/Laba2/Program.cs
@@ -0,0 +1,25 @@
+﻿using Laba2;
+
+double a3 = ReadCoeficcientFromConsole("a3");
+double a2 = ReadCoeficcientFromConsole("a2");
+double a1 = ReadCoeficcientFromConsole("a1");
+double a0 = ReadCoeficcientFromConsole("a0");
+
+CubicPolynomial polynomial = new CubicPolynomial(a3, a2, a1, a0);
+
+Console.Write("Введіть точку a відрізку [a, b] >: ");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Введіть точку b відрізку [a, b] >: ");
+double b = double.Parse(Console.ReadLine());
+
+Console.Write("Введіть рівень точності епсілон >: ");
+double epsilon = double.Parse(Console.ReadLine());
+
+double minValue = polynomial.FindMinimum(a, b, epsilon);
+Console.WriteLine($"Мінімальне значення на відрізку [{a}, {b}] з точністю {epsilon}: {minValue}");
+
+double ReadCoeficcientFromConsole(string coefName)
+{
+    Console.Write($"Введіть коефіцієнт {coefName}>: ");
+    return double.Parse(Console.ReadLine());
+}
