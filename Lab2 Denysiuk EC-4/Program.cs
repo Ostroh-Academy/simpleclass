d4d64621fab94bf4fb9ea81ab5c44300353478cb
Юлія Денисюк <yuliia.denysiuk@oa.edu.ua>
2024-03-24 21:55:39 +0200

    Виконана лабораторна робота №2. Денисюк ЕК-4

---
+++ Lab2 Denysiuk EC-4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal partial class Program
+    {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Введiть слово:");
+                string input = Console.ReadLine();
+
+                Word wordObject = new Word(input);
+                int digitCount = wordObject.CountDigits();
+
+                Console.WriteLine($"Кiлькiсть цифр у словi '{input}': {digitCount}");
+            Console.ReadKey();
+            }
+    }   
+}
