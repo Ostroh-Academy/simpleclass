78e1c76c507dee15caaf1743954412e747c09a04
Nastiia18 <145715374+Nastiia18@users.noreply.github.com>
2024-03-24 18:29:37 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  методу обчислення довжини вектора
+            Console.WriteLine("Введіть координати вектора (x y z):");
+            string[] input = Console.ReadLine().Split();
+            double x = double.Parse(input[0]);
+            double y = double.Parse(input[1]);
+            double z = double.Parse(input[2]);
+
+            Vector3d vector = new Vector3d(x, y, z);
+            double length = vector.CalculateLength();
+            Console.WriteLine($"Довжина вектора: {length}");
+        }
+    }
+}
---
+++ Vector3d.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Vector3d
+    {
+        private double x;
+        private double y;
+        private double z;
+
+        public Vector3d(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public double CalculateLength()
+        {
+            return Math.Sqrt(x * x + y * y + z * z);
+        }
+    }
+}
