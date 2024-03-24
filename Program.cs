4dfec495a225d1793aa46300de95dab2e1e75cd7
Iryna Krush <145108812+Ira-name@users.noreply.github.com>
2024-03-24 18:26:35 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Lab_2_patern;
+using System;
+
+
+class Program
+{
+	static void Main(string[] arg)
+	{
+		Console.WriteLine("Введіть координати вершин чотирикутника (x1, y1), (x2, y2), (x3, y3), (x4, y4):");
+		Quadranglecs[] vertices = new Quadranglecs[4];
+
+		for (int i = 0; i < 4; i++)
+		{
+			Console.Write($"Введіть координати вершини {i + 1}:");
+			string[] coordinates = Console.ReadLine().Split(' ');
+			double x = double.Parse(coordinates[0]);
+			double y = double.Parse(coordinates[1]);
+			vertices[i] = new Quadranglecs(x, y);
+		}
+
+		double perimeter = Quadranglecs.Perimeter(vertices);
+		Console.WriteLine($"Периметр чотирикутника: {perimeter}");
+	}
+}
---
+++ Quadranglecs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2_patern
+{
+	public class Quadranglecs
+	{
+		public double X { get; set; }
+		public double Y { get; set; }
+
+		public Quadranglecs(double x, double y)
+		{
+			X = x;
+			Y = y;
+		}
+
+		public static double Length(Quadranglecs p1, Quadranglecs p2)
+		{
+			double dx = p2.X - p1.X;
+			double dy = p2.Y - p1.Y;
+			return Math.Sqrt(dx * dx + dy * dy);
+		}
+
+		public static double Perimeter(Quadranglecs[] vertices)
+		{
+			double perimeter = 0;
+			int numVertices = vertices.Length;
+
+			for (int i = 0; i < numVertices; i++)
+			{
+				perimeter += Length(vertices[i], vertices[(i + 1) % numVertices]);
+			}
+
+			return perimeter;
+		}
+	}
+}
